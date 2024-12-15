a8f11e032abf966ee58f0e0f32cab01844d5ab71
Aravind-R-Warrier <aravindrwarrier1@gmail.com>
2024-12-15 15:25:47 +0530

    Added SignIn component firebase Auth And Db

--- src/Routes/SignIn/SignIn.jsx
+++ src/Routes/SignIn/SignIn.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { signInWithGooglePopUp,createUserDocumentFromAuth } from '../../utils/firebase/firebase'
+
 
 function SignIn() {
+  const logGoogleUser=async()=>{
+    const {user}=await signInWithGooglePopUp()
+   const userDocRef= await createUserDocumentFromAuth(user)
+  }
+
   return (
     <div>
       <h1>Sign In</h1>
+      <button onClick={logGoogleUser}>sign in popup</button>
     </div>
   )
 }
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,56 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// Authorisation
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
+// Db firebase-firestore
+import { getFirestore,getDoc,setDoc,doc } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyctpw4MrfDmjHlxvQpi4vKta8oJRSOTU",
+  authDomain: "crown-clothing-79fc0.firebaseapp.com",
+  projectId: "crown-clothing-79fc0",
+  storageBucket: "crown-clothing-79fc0.firebasestorage.app",
+  messagingSenderId: "443407543842",
+  appId: "1:443407543842:web:5ce12258e353b049fa647b"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// initialise Auth
+const provider=new GoogleAuthProvider()
+
+provider.setCustomParameters({
+    prompt:"select_account"
+})
+export const auth=getAuth()
+export const signInWithGooglePopUp=()=>signInWithPopup(auth,provider)
+
+// initialise db
+export const db=getFirestore()
+
+export const createUserDocumentFromAuth=async (userAuth)=>{
+const userDocRef=doc(db,'users',userAuth.uid)
+console.log(userDocRef)
+const userSnapShot=await getDoc(userDocRef)
+console.log(userSnapShot)
+console.log(userSnapShot.exists())
+
+// check wheather user instance exists or not
+if(!userSnapShot.exists()){
+  const {displayName,email}=userAuth;
+  const createdAt=new Date()
+  try{
+    await setDoc(userDocRef,{
+      displayName,
+      email,
+      createdAt
+    })
+  }catch(err){
+    console.log(err,'something wrong in function of usercreation')
+  }
+}
+return userDocRef,alert("already existing user,please ensure credentials")
+}
